62c6f901c05e53e5e289594401ec7ce956ca3fd8
Sitati Kituyi <sitati.kituyi@googlemail.com>
2013-06-14 11:43:57 +0300

    Preparing for 1.1-frontlinesms release

--- OneTimeDataGrailsPlugin.groovy
+++ OneTimeDataGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class OneTimeDataGrailsPlugin {
 	// the plugin version
-	def version = "1.1-frontlinesms-SNAPSHOT"
+	def version = "1.1-frontlinesms"
 		// the version or versions of Grails the plugin is designed for
 		def grailsVersion = "1.1 > *"
 		// the other plugins this plugin depends on
